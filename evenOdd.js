e8386087e071179342ad2ad256d14f9befa7ac86
calvinscharf <scharfcalvin@gmail.com>
2022-05-30 16:58:04 -0500

    added onclick function, moved const to inside function

--- evenOdd.js
+++ evenOdd.js
@@ -1,22 +1,30 @@
  let usersWord =""
-let oddList = document.getElementById("odd")
-let evenList =document.getElementById("even")
+
 
 function updateWord(element){
      usersWord = element.value
-     
-     console.log(usersWord)
+    
+    //  console.log(usersWord)
  }
 
 function handleSubmit() {
-    
-    let newListItem = document.createElement('LI')
+   const oddList = document.getElementById("odd")
+    const evenList = document.getElementById("even")
+   
+    // this line creates a new LI Element. We'll do this every time a word is submitted so this should stay the same
+    let newListItem = document.createElement('li')
+    // this line sticks the user's word into the newly create LI Element so we'll keep this the same, as well.
     newListItem.innerText = usersWord
      
-    if (wordlength=even) {
-        console.log(even)
+    if(usersWord.length % 2 === 0) {
+        // console.log("even",evenList)
+        evenList.appendChild(newListItem)
+        
+    } else{
+     // this line attaches/appends the element to the Odd List !!!THIS IS WHERE THE PROGRAM SHOULD DECIDE IF IT'S EVEN OR ODD!!!
+    oddList.appendChild(newListItem)   
     }
-    oddList.appendChild(newListItem)
+    
 
     usersWord = ""
     document.getElementById("even-odd-form").reset()
